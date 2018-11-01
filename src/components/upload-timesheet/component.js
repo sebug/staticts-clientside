46f09c7e3b1a76fcd1c4c8a17c7aa14ba58ef810
sebug <sebastian.gfeller@gmail.com>
2018-11-01 14:09:40 +0100

    Adding upload component

--- src/components/upload-timesheet/component.js
+++ src/components/upload-timesheet/component.js
@@ -1,11 +1,9 @@
 import ko from 'knockout';
 import htmlContent from './component.html';
-import uploadTimesheet from '../upload-timesheet/component.js';
-
-ko.components.register('upload-timesheet', uploadTimesheet);
 
 class ViewModel {
     constructor(params) {
+	
     }
 }
 
