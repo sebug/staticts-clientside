33ef4b9d0ad210938f5119b6a44ad7600113d34e
sebug <sebastian.gfeller@gmail.com>
2018-11-04 16:41:26 +0100

    Also adding a specrunner file

---
+++ src/specRoot.js
@@ -0,0 +1,5 @@
+describe('the root module', function () {
+    it('is an object', function () {
+	expect({}).toEqual(jasmine.any(Number));
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
+const JasmineWebpackPlugin = require('jasmine-webpack-plugin');
 
 let mainConfig = {
     mode: 'production',
@@ -23,4 +24,24 @@ let mainConfig = {
     ]
 };
 
-module.exports = [ mainConfig ];
+const testConfig = {
+    mode: 'production',
+    entry: path.join(__dirname, '/src/specRoot.js'),
+    output: {
+        filename: 'specRoot.js',
+        path: path.join(__dirname, '/dist')},
+    module:{
+        rules:[{
+           test: /\.js$/,
+           exclude: /node_modules/,
+           loader: 'babel-loader'
+        }, {
+	    test: /\.html$/, use: [ 'html-loader' ]
+	}]
+    },
+    plugins:[
+	new JasmineWebpackPlugin()
+    ]
+};
+
+module.exports = [ mainConfig, testConfig ];
