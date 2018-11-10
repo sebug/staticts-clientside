cba7ef750603539a98bcec7c662ab9ea02876e01
sebug <sebastian.gfeller@gmail.com>
2018-11-10 15:52:43 +0100

    Adding regenerator polyfill to tests as well

--- src/specRoot.js
+++ src/specRoot.js
@@ -1,3 +1,4 @@
+import babelPolyfill from 'babel-polyfill';
 import appRootSpec from './components/app-root/componentSpec.js';
 import uploadTimesheetComponentSpec from './components/upload-timesheet/componentSpec.js';
 
