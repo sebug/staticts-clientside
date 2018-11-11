86ee58407e06e54e7b92c62e1283b6ed713e4731
sebug <sebastian.gfeller@gmail.com>
2018-11-11 20:05:36 +0100

    Ready for binding

--- src/components/follow-up/component.js
+++ src/components/follow-up/component.js
@@ -5,6 +5,7 @@ class ViewModel {
     constructor(params) {
 	this.followUpCode = ko.observable();
 	this.getData = this.getData.bind(this);
+	this.followUp = ko.observable();
         if (document && document.location) {
             let sps = (new URL(document.location)).searchParams;
             let followUpCode = sps.get('followUpCode');
@@ -17,7 +18,13 @@ class ViewModel {
     async getData() {
 	const followUpResult = await fetch('/api/FollowUpTrigger?code=' + this.followUpCode());
 	const followUp = await followUpResult.json();
-	console.log(followUp);
+	if (followUp && followUp.FollowUpLines) {
+	    for (let line of followUp.FollowUpLines) {
+		line.RemainingWork = Math.max(0, ((line.BaseLine || 0) - line.TotalTimeSpent));
+                line.Variance = line.TotalTimeSpent + line.RemainingWork - (line.BaseLine || 0);
+	    }
+	}
+	this.followUp(followUp);
     }
 }
 
