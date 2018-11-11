4e2b5f673ffa43e68ab7408f638261cb5ec917e9
sebug <sebastian.gfeller@gmail.com>
2018-11-11 19:55:01 +0100

    Making the get data call

--- src/components/follow-up/component.js
+++ src/components/follow-up/component.js
@@ -4,12 +4,20 @@ import htmlContent from './component.html';
 class ViewModel {
     constructor(params) {
 	this.followUpCode = ko.observable();
+	this.getData = this.getData.bind(this);
         if (document && document.location) {
             let sps = (new URL(document.location)).searchParams;
             let followUpCode = sps.get('followUpCode');
-            this.followupCode(followUpCode);
+            this.followUpCode(followUpCode);
 	    console.log('Follow up code is ' + this.followUpCode());
+	    this.getData();
         }
+    }
+
+    async getData() {
+	const followUpResult = await fetch('/api/FollowUpTrigger?code=' + this.followUpCode());
+	const followUp = await followUpResult.json();
+	console.log(followUp);
     }
 }
 
