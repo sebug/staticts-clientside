35593ecb11361f7c0bbe43e50a39fad66cdd11d8
sebug <sebastian.gfeller@gmail.com>
2018-11-11 19:49:38 +0100

    Mentioning other entry variable

--- src/followup.js
+++ src/followup.js
@@ -1,5 +1,5 @@
 import ko from 'knockout';
 import followupRoot from './components/follow-up/component.js';
-ko.components.register('follow-up', appRoot);
+ko.components.register('follow-up', followupRoot);
 
 ko.applyBindings({}, document.getElementsByTagName('main')[0]);
