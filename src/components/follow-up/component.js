e1ea52838092b007c792c503a546f5a14772bfab
sebug <sebastian.gfeller@gmail.com>
2018-11-11 20:35:47 +0100

    It was actually TimeSpent

--- src/components/follow-up/component.js
+++ src/components/follow-up/component.js
@@ -26,7 +26,7 @@ class ViewModel {
                 line.Variance = line.TotalTimeSpent + line.RemainingWork - (line.BaseLine || 0);
 		line.Project = line.Project || '';
 		line.Task = line.Task || '';
-		line.DateBuckets = line.DateBuckets || {};
+		line.TimeSpent = line.TimeSpent || {};
 	    }
 	}
 	this.followUp(followUp);
