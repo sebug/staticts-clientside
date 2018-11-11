be37193c7263119934ad1027050bd4335b587a33
sebug <sebastian.gfeller@gmail.com>
2018-11-11 10:13:53 +0100

    Using xlsx

--- src/components/upload-job/component.js
+++ src/components/upload-job/component.js
@@ -1,5 +1,24 @@
 import ko from 'knockout';
 import htmlContent from './component.html';
+import XLSX from 'xlsx';
+
+function readAsArrayBuffer(file) {
+    return new Promise((resolve, reject) => {
+	const fr = new FileReader();
+        fr.onload = e => {
+            resolve(e.target.result);
+        };
+        fr.onerror = () => {
+            reject(fr.error);
+        };
+        fr.readAsArrayBuffer(file);
+    });
+}
+
+async function readJob(file) {
+    const ab = readAsArrayBuffer(file);
+    return ab;
+}
 
 class ViewModel {
     constructor(params) {
@@ -9,6 +28,9 @@ class ViewModel {
 
     takeFile(file) {
 	console.log(file);
+	readJob(file).then(job => {
+	    console.log(job);
+	});
     }
 
     upload() {
